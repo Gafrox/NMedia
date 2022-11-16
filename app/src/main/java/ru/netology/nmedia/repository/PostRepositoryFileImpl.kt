9d353a44fde05130472e47720a980071324a025f
Sergey Baranov <thx4win@gmail.com>
2022-11-16 18:34:55 +0300

    Storage

---
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepositoryFileImpl.kt
@@ -0,0 +1,104 @@
+package ru.netology.nmedia.repository
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import ru.netology.nmedia.dto.Post
+
+class PostRepositoryFileImpl(
+    private val context: Context
+) : PostRepository {
+    private val currentAuthor = "Netology"
+    private val currentTime = "Now"
+    private var nextId = 1L
+    private val gson = Gson()
+    private val type = TypeToken.getParameterized(List::class.java, Post::class.java).type
+    private val filename = "posts.json"
+    private var posts = List(5) {
+        Post(
+            id = nextId++,
+            author = "Нетология. Университет интернет-профессий будущего",
+            content = "ID:${nextId.dec()} Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
+            published = "21 мая в 18:36",
+            likedByMe = false,
+            likes = 999,
+            shares = 1_099_998,
+            views = 439,
+            video = "https://youtu.be/EQ_btxhpRzU"
+        )
+    }
+    private val data = MutableLiveData(posts)
+
+    init {
+        val file = context.filesDir.resolve(filename)
+        if (file.exists()) {
+            context.openFileInput(filename).bufferedReader().use {
+                posts = gson.fromJson(it, type)
+                data.value = posts
+            }
+            posts.forEach {
+                nextId = maxOf(it.id, nextId)
+            }
+            nextId++
+        } else sync()
+    }
+
+    override fun get(): LiveData<List<Post>> = data
+
+    override fun likeById(id: Long) {
+        posts = posts.map {
+            if (it.id != id) it else it.copy(
+                likedByMe = !it.likedByMe, likes = if (it.likedByMe) it.likes - 1 else it.likes + 1
+            )
+        }
+        data.value = posts
+        sync()
+    }
+
+    override fun shareById(id: Long) {
+        posts = posts.map {
+            if (id == it.id) {
+                it.copy(shares = it.shares + 1)
+            } else {
+                it
+            }
+        }
+        data.value = posts
+        sync()
+    }
+
+    override fun deleteById(id: Long) {
+        posts = posts.filter { it.id != id }
+        data.value = posts
+        sync()
+    }
+
+    override fun save(post: Post) {
+        if (post.id == 0L) {
+            posts = listOf(
+                post.copy(
+                    id = nextId++,
+                    author = currentAuthor,
+                    likedByMe = false,
+                    published = currentTime
+                )
+            ) + posts
+            data.value = posts
+            sync()
+        }
+
+        posts = posts.map {
+            if (it.id != post.id) it else it.copy(content = post.content)
+        }
+        data.value = posts
+        sync()
+    }
+
+    private fun sync() {
+        context.openFileOutput(filename, Context.MODE_PRIVATE).bufferedWriter().use {
+            it.write(gson.toJson(posts))
+        }
+    }
+}
